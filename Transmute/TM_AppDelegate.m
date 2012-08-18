82a3a7d1f30a403e6b19fcc58fabf84ee9f6bc5e
Mihir Singh <mihir@royalpanda.co>
2012-08-18 09:36:56 -0700

    Initial Commit

---
+++ Transmute/TM_AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  TM_AppDelegate.m
+//  Transmute
+//
+//  Created by Mihir Singh on 8/18/12.
+//  Copyright (c) 2012 The Royal Panda Company. All rights reserved.
+//
+
+#import "TM_AppDelegate.h"
+
+@implementation TM_AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
